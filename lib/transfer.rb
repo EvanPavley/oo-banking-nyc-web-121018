76053756c6dff1c39f05aafc924883ecad34b06f
EvanPavley <github email address>
2018-12-13 21:01:13 -0500

    tests passing

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,40 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    sender.valid? && receiver.valid? ? true : false
+  end
+
+  def execute_transaction
+    #adds the amount to the receiver's account
+    #subtracts the amount from the sender's balance
+
+    if sender.balance > amount && @status == "pending"
+      receiver.deposit(@amount)
+      sender.withdrawal(@amount)
+      @status = "complete"
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if receiver.balance > amount && @status == "complete"
+      receiver.withdrawal(@amount)
+      sender.deposit(@amount)
+      @status = "reversed"
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
 end
